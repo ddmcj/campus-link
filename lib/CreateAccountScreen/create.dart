357c74c93183d7ee5590e58f9a0ef06b4c18c854
ddmcj <126475019+ddmcj@users.noreply.github.com>
2023-03-16 21:45:03 +0800

    Update create.dart

--- lib/CreateAccountScreen/create.dart
+++ lib/CreateAccountScreen/create.dart
@@ -14,7 +14,7 @@ Feature description: Users are able to input texts necessary for account creatio
 
 
 class CreateAccount extends StatelessWidget {
-  const CreateAccount ({Key? key}) : super(key: key);
+  const CreateAccount({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,35 +22,23 @@ class CreateAccount extends StatelessWidget {
       body: SingleChildScrollView(
         child: Center(
           child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Center(
-              child: Image.asset('assets/images/campuslinks.png', width: 215),),
-            
-            const SizedBox(height:50),
-            const Text(
-              'Create Account',
-              style:TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-                color: Colors.white,
-              )
-            ),
-            const SizedBox(height: 50),
-             const LoginField(hintText: 'Email'),
-            const SizedBox(height: 20),
-             const PassWordField(hintText: 'Password'),
-            const SizedBox(height: 20),
-             const LoginField(hintText: 'Name'),
-            const SizedBox(height: 50),
-
-            const SocialButton(),
-           
-
-            
-
-          ],))
-      )
+            children: [
+              const SizedBox(height: 20),
+              Center(child: Image.asset('assets/images/campuslinks.png', width: 215)),
+              const SizedBox(height: 50),
+              const Text('Create Account', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white)),
+              const SizedBox(height: 50),
+              LoginField(hintText: 'Name', iconData: Icons.person), // <-- add the prefixIcon parameter
+              const SizedBox(height: 20),
+              PassWordField(hintText: 'Password'),
+              const SizedBox(height: 20),
+              LoginField(hintText: 'Email', iconData: Icons.email), // <-- add the prefixIcon parameter
+              const SizedBox(height: 50),
+              const SocialButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
-} 
+}
